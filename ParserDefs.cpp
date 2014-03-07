ac5c21a012900db70b38e16b69df033cd53e0f0c
caebli3 <caebli3@gmail.com>
2014-03-07 17:12:02 -0500

    Edited parser

--- ParserDefs.cpp
+++ ParserDefs.cpp
@@ -18,21 +18,9 @@ const reduce_t Parser::reduceTable = Parser::initReduceTable();
 const action_t Parser::initActionTable() {
 	// The rest will automatically init to 0 (X/ERROR)
 	action_t table(numStates);
-	// Grammar 02
-	/*
-	table[0][NT_S] = ACCEPT;
-	table[0][NT_A] = 1;
-	table[0][T_int] = 2;
-	table[0][T_end] = ACCEPT;
-	table[1][T_end] = REDUCE;
-	table[2][T_plus] = 3;
-	table[2][T_end] = REDUCE;
-	table[3][NT_A] = 4;
-	table[3][T_int] = 2;
-	table[4][T_end] = REDUCE;
-	*/
 	
 	// Grammar 02
+	// Ahhhhh seg fault!
 	table[0][NT_S] = ACCEPT;
 	table[0][NT_A] = 1;
 	table[0][NT_B] = 2;
@@ -68,7 +56,6 @@ const action_t Parser::initActionTable() {
 	table[10][T_plus] = REDUCE;
 	table[10][T_rparen] = REDUCE;
 	table[10][T_end] = REDUCE;
-	
 	
 	return table;
 }
